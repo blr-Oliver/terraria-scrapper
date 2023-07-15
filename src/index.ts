1bd473cb35e6be1f8adfe4d51f6abf5f6400d795
blr-Oliver <vasily.liaskovsky@gmail.com>
2023-07-15 23:22:05 +0300

    detailed weapon list

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import * as fs from 'fs';
 import {getWeaponList} from './weapon-list';
 
-getWeaponList().then(names => {
-  fs.writeFileSync('out/out.json', JSON.stringify(names), {encoding: 'utf8'});
-});
+async function keypress(): Promise<void> {
+  process.stdin.setRawMode(true);
+
+  return new Promise(resolve => {
+    process.stdin.once('data', () => {
+      process.stdin.setRawMode(false);
+      resolve();
+    });
+  });
+}
+
+async function executeProgram(): Promise<void> {
+  console.log('Press any key to continue');
+  await keypress();
+  let weapons = await getWeaponList();
+  fs.writeFileSync('out/out.json', JSON.stringify(weapons), {encoding: 'utf8'});
+}
+executeProgram().then(() => process.exit(0));
