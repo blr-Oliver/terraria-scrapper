458a9f6d4c5d60a478154f69aec1603254bc1d83
blr-Oliver <vasily.liaskovsky@gmail.com>
2023-08-08 09:58:10 +0300

    extract Varying type core

--- src/platform-varying.ts
+++ src/platform-varying.ts
@@ -1,3 +1,5 @@
+import {Varying, VaryingValue} from './varying';
+
 export enum Platform {
   pc = 1,
   console = 2,
@@ -10,44 +12,8 @@ export type PlatformName = keyof typeof Platform;
 export type PlatformList = PlatformName[];
 export const ALL_PLATFORMS: ReadonlyArray<PlatformName> = ['pc', 'console', 'oldGen', 'mobile', 'threeDS'];
 
-export type PlatformVaryingValue<T> = {
-  [key in PlatformName]?: T
-}
-
-export type RequiredFields<T> = keyof {
-  [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
-}
-export type OptionalFields<T> = keyof {
-  [K in keyof T as T[K] extends Required<T>[K] ? never : K]: T[K]
-}
-export type ObjectFields<T> = keyof {
-  [K in keyof T as Exclude<T[K], undefined> extends object ? K : never]: T[K]
-}
-export type PrimitiveFields<T> = keyof {
-  [K in keyof T as Exclude<T[K], undefined> extends object ? never : K]: T[K]
-}
-
-type RequiredObjectFields<T> = RequiredFields<T> & ObjectFields<T>;
-type RequiredPrimitiveFields<T> = RequiredFields<T> & PrimitiveFields<T>;
-type OptionalObjectFields<T> = OptionalFields<T> & ObjectFields<T>;
-type OptionalPrimitiveFields<T> = OptionalFields<T> & PrimitiveFields<T>;
-
-//workaround to avoid distributed conditional types
-export type PlatformVaryingObject<T extends object> = {
-  [K in RequiredObjectFields<T>]: PlatformVarying<T[K]>;
-} & {
-  [K in RequiredPrimitiveFields<T>]: PlatformVaryingValue<T[K]>;
-} & {
-  [K in OptionalObjectFields<T>]?: PlatformVarying<Exclude<T[K], undefined>>;
-} & {
-  [K in OptionalPrimitiveFields<T>]?: PlatformVaryingValue<Exclude<T[K], undefined>>;
-}
-
-export type PlatformVarying<T> =
-    T extends Array<infer E> ? Array<PlatformVarying<E>> :
-        T extends Function ? PlatformVaryingValue<T> :
-            T extends object ? PlatformVaryingObject<T> :
-                PlatformVaryingValue<T>;
+export type PlatformVaryingValue<T> = VaryingValue<T, PlatformName>;
+export type PlatformVarying<T> = Varying<T, PlatformName>;
 
 export function makeVarying<T>(value: T, platforms: PlatformName[] = ALL_PLATFORMS as PlatformName[]): PlatformVaryingValue<T> {
   let result: PlatformVaryingValue<T> = {};
---
+++ src/varying.ts
@@ -0,0 +1,38 @@
+export type VaryingValue<T, K extends keyof any> = {
+  [key in K]?: T
+}
+
+export type RequiredFields<T> = keyof {
+  [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
+}
+export type OptionalFields<T> = keyof {
+  [K in keyof T as T[K] extends Required<T>[K] ? never : K]: T[K]
+}
+export type ObjectFields<T> = keyof {
+  [K in keyof T as Exclude<T[K], undefined> extends object ? K : never]: T[K]
+}
+export type PrimitiveFields<T> = keyof {
+  [K in keyof T as Exclude<T[K], undefined> extends object ? never : K]: T[K]
+}
+
+export type RequiredObjectFields<T> = RequiredFields<T> & ObjectFields<T>;
+export type RequiredPrimitiveFields<T> = RequiredFields<T> & PrimitiveFields<T>;
+export type OptionalObjectFields<T> = OptionalFields<T> & ObjectFields<T>;
+export type OptionalPrimitiveFields<T> = OptionalFields<T> & PrimitiveFields<T>;
+
+//workaround to avoid distributed conditional types
+export type VaryingObject<T extends object, K extends keyof any> = {
+  [KK in RequiredObjectFields<T>]: Varying<T[KK], K>;
+} & {
+  [KK in RequiredPrimitiveFields<T>]: VaryingValue<T[KK], K>;
+} & {
+  [KK in OptionalObjectFields<T>]?: Varying<Exclude<T[KK], undefined>, K>;
+} & {
+  [KK in OptionalPrimitiveFields<T>]?: VaryingValue<Exclude<T[KK], undefined>, K>;
+}
+
+export type Varying<T, K extends keyof any> =
+    T extends Array<infer E> ? Array<Varying<E, K>> :
+        T extends Function ? VaryingValue<T, K> :
+            T extends object ? VaryingObject<T, K> :
+                VaryingValue<T, K>;
