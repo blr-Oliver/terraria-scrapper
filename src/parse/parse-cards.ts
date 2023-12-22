f5a4ad6bfa3506b9d2bcff79d6317a3c01151d4d
blr-Oliver <vasily.liaskovsky@gmail.com>
2023-12-22 15:10:23 +0300

    sub-cards (same name, different platforms) merged into single card

--- src/parse/parse-cards.ts
+++ src/parse/parse-cards.ts
@@ -3,7 +3,7 @@ import {ShortInfoCollection} from '../analyze/ShortInfoCollector';
 import {EntryInfo} from '../execution';
 import {ensureExists} from '../fetch/common';
 import {normalizeFileName} from '../fetch/fetch';
-import {ALL_PLATFORMS, PlatformName} from '../platform-varying';
+import {ALL_PLATFORMS, PlatformList, PlatformName, PlatformVaryingValue} from '../platform-varying';
 import {loadDocument} from './common';
 import {extractPlatformsFromImages} from './extract-varying';
 import {MetaInfo, parseItemFromCard, ScrappedWeapon} from './parse-item';
@@ -33,7 +33,7 @@ export function parseSinglePage(document: Document): ScrappedWeapon[] {
   let messageBox = contentRoot.querySelector('.message-box.msgbox-color-blue');
   let platforms: PlatformName[] = messageBox ? extractPlatformsFromImages(messageBox) : ALL_PLATFORMS as PlatformName[];
   let cardBlocks = contentRoot.querySelectorAll('.infobox.item');
-  return Array.prototype.map.call(cardBlocks, block => {
+  let result = Array.prototype.map.call(cardBlocks, block => {
     let meta: MetaInfo = {
       platforms: platforms.slice(),
       parsingExceptions: []
@@ -42,4 +42,43 @@ export function parseSinglePage(document: Document): ScrappedWeapon[] {
     (weaponInfo as ScrappedWeapon).meta = meta;
     return weaponInfo as ScrappedWeapon;
   }) as ScrappedWeapon[];
+  return maybeMerge(result);
+}
+
+function maybeMerge(items: ScrappedWeapon[]): ScrappedWeapon[] {
+  let collection: { [name: string]: ScrappedWeapon } = {};
+  let hasCollision = false;
+  for (let item of items) {
+    const platforms = item.meta!.platforms;
+    const primaryPlatform = platforms[0];
+    const name = item.name[primaryPlatform]!;
+    if (!(name in collection)) {
+      collection[name] = item;
+    } else {
+      hasCollision = true;
+      mergeInto(collection[name], item, platforms);
+    }
+  }
+  if (!hasCollision) return items;
+  let result: ScrappedWeapon[] = [];
+  for (let name in collection)
+    result.push(collection[name]);
+  return result;
+}
+
+function mergeInto(dest: ScrappedWeapon, src: ScrappedWeapon, platforms: PlatformList) {
+  for (let property in src) {
+    if (property === 'meta') continue;
+    const srcElement: PlatformVaryingValue<any> = (src as any)[property];
+    if (property in dest) {
+      const destElement: PlatformVaryingValue<any> = (dest as any)[property];
+      for (let platform of platforms) {
+        destElement[platform] = srcElement[platform];
+      }
+    } else {
+      (dest as any)[property] = srcElement;
+    }
+  }
+  dest.meta!.platforms.push(...platforms);
+  dest.meta!.parsingExceptions.push(...src.meta!.parsingExceptions);
 }
