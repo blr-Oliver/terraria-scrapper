b262855d904e1685d3731d36f92e04f15919db29
blr-Oliver <vasily.liaskovsky@gmail.com>
2023-12-30 06:21:16 +0300

    shorter card index

--- src/analyze/collect-cards.ts
+++ src/analyze/collect-cards.ts
@@ -1,24 +1,18 @@
 import * as fs from 'fs';
 import {ItemCard, ScrappedItem} from '../common/types';
+import {sortKeys} from '../common/utils';
 import {EntryInfo} from '../execution';
 import {normalizeFileName} from '../fetch/fetch';
-import {PlatformName, PlatformVaryingValue} from '../platform-varying';
+import {ALL_PLATFORMS, PlatformVarying} from '../platform-varying';
 import {ItemShortInfo} from './ShortInfoBuilder';
 
-export interface CardLocation {
+export interface CardRecord {
   name: string;
+  commonName?: string;
+  pageTitle?: string;
   fileName: string;
-  itemIndex: number
-}
-
-export interface CardIndex {
-  cards: {
-    [key: string]: CardLocation
-  };
-  multiCards: string[];
-  multiCardRefs: {
-    [key: string]: string
-  };
+  index: number;
+  exceptions?: any;
 }
 
 export async function collectCards(entry: EntryInfo): Promise<void> {
@@ -39,48 +33,86 @@ export async function collectCards(entry: EntryInfo): Promise<void> {
   await Promise.allSettled(queue);
   collector.finish();
 
-  return fs.promises.writeFile(`${entry.out}/json/cardIndex.json`, JSON.stringify(collector, null, 2), {encoding: 'utf8'});
+  return fs.promises.writeFile(`${entry.out}/json/cardIndex.json`, JSON.stringify(collector.data, null, 2), {encoding: 'utf8'});
 }
 
 
-class CardIndexCollector implements CardIndex {
-  cards: { [key: string]: CardLocation } = {};
-  multiCards: string[] = [];
-  multiCardRefs: { [key: string]: string } = {};
-  multiCardsSet?: Set<string> = new Set<string>();
+class CardIndexCollector {
+
+  data: { [itemName: string]: CardRecord } = {};
 
   collect(itemName: string, fileName: string, page: ScrappedItem[]) {
-    for (let i = 0; i < page.length; ++i) {
-      const card = page[i];
-      const primaryPlatform = this.getPrimaryPlatform(card);
-      let cardName: string | string[] = card.name;
-      if (this.isMultiCard(card, primaryPlatform)) {
-        this.multiCardsSet!.add(cardName);
-        this.multiCardRefs[itemName] = cardName;
-      }
-      this.cards[cardName] = {
-        name: cardName,
+    for (let i = 0; i < page.length; i++) {
+      let item = page[i];
+      this.collectSingleItem(itemName, fileName, item, i, page.length === 1);
+    }
+  }
+
+  private collectSingleItem(itemName: string, fileName: string, item: ScrappedItem, index: number, checkName: boolean) {
+    const card = item.item;
+    const pageTitle = card.pageTitle[item.platforms[0]]!;
+    if (checkName && (itemName !== item.name || itemName != pageTitle || item.name !== pageTitle) || this.hasMultiId(card)) {
+      this.collectMultiCard(itemName, fileName, pageTitle, item, index);
+    } else {
+      this.collectNormalCard(itemName, fileName, index);
+    }
+  }
+
+  private collectNormalCard(itemName: string, fileName: string, index: number) {
+    if (!(itemName in this.data)) {
+      this.data[itemName] = {
+        name: itemName,
         fileName,
-        itemIndex: i
+        index
       }
     }
   }
 
-  isMultiCard(item: ScrappedItem, primaryPlatform: PlatformName): boolean {
-    const itemCard = item.item;
-    return ['id', 'name'].some(property => {
-      if (!(property in itemCard)) return false;
-      let value = itemCard[property as keyof ItemCard] as PlatformVaryingValue<any>;
-      return Array.isArray(value[primaryPlatform]);
-    });
+  private collectMultiCard(itemName: string, fileName: string, pageTitle: string, item: ScrappedItem, index: number) {
+    let oldRecord = this.data[itemName];
+    if (oldRecord) {
+      this.setWithNoConflicts(oldRecord, 'commonName', item.name);
+      this.setWithNoConflicts(oldRecord, 'pageTitle', pageTitle);
+      this.setWithNoConflicts(oldRecord, 'index', index);
+    } else {
+      this.data[itemName] = {
+        name: itemName,
+        commonName: item.name,
+        pageTitle: pageTitle,
+        fileName,
+        index: index
+      }
+    }
   }
 
-  getPrimaryPlatform(itemCard: ScrappedItem): PlatformName {
-    return itemCard.platforms[0];
+  private setWithNoConflicts<K extends keyof CardRecord>(oldRecord: CardRecord, property: K, value: CardRecord[K]) {
+    if (!oldRecord[property]) {
+      oldRecord[property] = value;
+    } else if (oldRecord[property] !== value)
+      this.addException(oldRecord, `conflicting ${property}`, value);
   }
+
+  private hasMultiId(card: PlatformVarying<ItemCard>): boolean {
+    if (!card.id) return false;
+    const idBundle = card.id!;
+    for (let platform of ALL_PLATFORMS) {
+      if (platform in idBundle) {
+        let value = idBundle[platform]!;
+        if (Array.isArray(value) && value.length > 1)
+          return true;
+      }
+    }
+    return false;
+  }
+
+  private addException(record: CardRecord, key: string, value: string) {
+    if (!record.exceptions) record.exceptions = {};
+    if (!(key in record.exceptions)) record.exceptions[key] = [];
+    (record.exceptions[key] as string[]).push(value);
+  }
+
 
   finish() {
-    this.multiCards = [...this.multiCardsSet!.keys()];
-    delete this.multiCardsSet;
+    this.data = sortKeys(this.data);
   }
 }
