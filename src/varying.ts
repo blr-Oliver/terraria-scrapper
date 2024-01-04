ca9083a4e97793aa2c84e100abe21119953378e6
blr-Oliver <vasily.liaskovsky@gmail.com>
2024-01-05 02:14:46 +0300

    varying on any depth abandoned for simpler single-level approach

--- src/varying.ts
+++ src/varying.ts
@@ -1,38 +1,13 @@
-export type VaryingValue<T, K extends keyof any> = {
-  [key in K]?: T
+export type VaryingValue<T, U extends keyof any> = {
+  [key in U]?: T
 }
 
-export type RequiredFields<T> = keyof {
-  [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
-}
-export type OptionalFields<T> = keyof {
-  [K in keyof T as T[K] extends Required<T>[K] ? never : K]: T[K]
-}
-export type ObjectFields<T> = keyof {
-  [K in keyof T as Exclude<T[K], undefined> extends object ? K : never]: T[K]
-}
-export type PrimitiveFields<T> = keyof {
-  [K in keyof T as Exclude<T[K], undefined> extends object ? never : K]: T[K]
+export type VaryingObject<T extends object, U extends keyof any> = {
+  [K in keyof T]: VaryingValue<T[K], U>;
 }
 
-export type RequiredObjectFields<T> = RequiredFields<T> & ObjectFields<T>;
-export type RequiredPrimitiveFields<T> = RequiredFields<T> & PrimitiveFields<T>;
-export type OptionalObjectFields<T> = OptionalFields<T> & ObjectFields<T>;
-export type OptionalPrimitiveFields<T> = OptionalFields<T> & PrimitiveFields<T>;
-
-//workaround to avoid distributed conditional types
-export type VaryingObject<T extends object, K extends keyof any> = {
-  [KK in RequiredObjectFields<T>]: Varying<T[KK], K>;
-} & {
-  [KK in RequiredPrimitiveFields<T>]: VaryingValue<T[KK], K>;
-} & {
-  [KK in OptionalObjectFields<T>]?: Varying<Exclude<T[KK], undefined>, K>;
-} & {
-  [KK in OptionalPrimitiveFields<T>]?: VaryingValue<Exclude<T[KK], undefined>, K>;
-}
-
-export type Varying<T, K extends keyof any> =
-    T extends Array<infer E> ? VaryingValue<Array<E>, K> :
-        T extends Function ? VaryingValue<T, K> :
-            T extends object ? VaryingObject<T, K> :
-                VaryingValue<T, K>;
+export type Varying<T, U extends keyof any> =
+    T extends Array<infer E> ? VaryingValue<Array<E>, U> :
+        T extends Function ? VaryingValue<T, U> :
+            T extends object ? VaryingObject<T, U> :
+                VaryingValue<T, U>;
